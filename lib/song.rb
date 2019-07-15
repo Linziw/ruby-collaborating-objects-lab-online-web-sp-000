a446bb4673e2579c4203fdf92d4fcf48148a6d10
Lindsey Wells <lindsey.wells84@gmail.com>
2019-07-15 11:11:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,13 +11,13 @@ def self.new_by_filename(file_name)
   artist, song_name = file_name.split(" - ")
   new_song = self.new(song_name)
   new_song.artist_name=(artist)
+  new_song
   
 end
 
 def self.artist_name=(name)
    self.artist = Artist.find_or_create_by_name(name)
    self.artist.add_song(self)
-   binding.pry
 end
  
 end
